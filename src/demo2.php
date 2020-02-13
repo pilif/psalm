de2c98bc6c1a76677c0bae9fb9986f07af413dcd
Philip Hofstetter <phofstetter@sensational.ch>
2020-02-13 08:28:31 +0100

    fix issue to show now the function call appears

--- src/demo2.php
+++ src/demo2.php
@@ -4,7 +4,7 @@ function global_fn(): string {
     return 'global';
 }
 
-function generateTemplateStrings(array $arr)
+function generateTemplateStrings(array $arr): array
 {
     return array_map(fn(string $e) => $e.global_fn(), $arr);
 }
